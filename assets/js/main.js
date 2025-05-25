e63ac725790ca915d0476923ada7ddf326f7626b
vivian-trangdaihong <thon0014@student.monash.edu>
2025-05-25 22:12:50 +1000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,6 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-// import Swiper JS
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-
-// import Swiper styles
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/modules/navigation.min.css';
 (function($) {
 			const swiper = new Swiper(".mySwiper", {
 				loop: true,
@@ -33,6 +26,6 @@ import 'swiper/modules/navigation.min.css';
 				},
 			  });
 
-})(jQuery);
+});
 
   
